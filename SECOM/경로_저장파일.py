5c1bbf49765be8ecb4f0b529a293d2424da9fc94
quasi_Baaam <yrtny70127@gmail.com>
2026-02-05 16:08:58 +0900

    Add initial script for checking current working directory and file existence

---
+++ SECOM/경로_저장파일.py
@@ -0,0 +1,11 @@
+김태훈
+import os
+os.chdir("/home/tae-hun-kim/바탕화면/AI모델개발/Semiconductor_Quality_Prospect")
+print("cwd:", os.getcwd())
+print("exists:", os.path.exists("SECOM/uci-secom.csv"))
+
+최정빈
+import os
+os.chdir(r"C:/Users/0726b/Semiconductor_Quality_Prospect")  
+print("cwd:", os.getcwd())
+print("exists:", os.path.exists("SECOM/uci-secom.csv"))
